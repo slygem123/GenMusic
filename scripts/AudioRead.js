fff329fc4a62047aabcf82ce65411e6ee94faea5
slygem123 <albin@grenhall.se>
2025-09-14 22:15:27 +0200

    Update AudioRead.js

--- scripts/AudioRead.js
+++ scripts/AudioRead.js
@@ -1,8 +1,9 @@
-return {
-    {
-        title = "Dreams",
-        artist = "Joakim Karud",
-        cover = "AudioRead/cover1.png",
-        src = "GenMusicAudioFiles/RomanReloaded.mp3"
-    },
-}
+// scripts/AudioRead.js
+const songs = [
+  {
+    title: "Dreams",
+    artist: "Joakim Karud",
+    cover: "AudioRead/cover1.png",
+    src: "GenMusicAudioFiles/RomanReloaded.mp3"
+  }
+];
