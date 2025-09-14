e4a2104dc30bb50f9b2957e7a26f748ae7e475ed
slygem123 <albin@grenhall.se>
2025-09-14 23:22:55 +0200

    Update AudioRead.js

--- scripts/AudioRead.js
+++ scripts/AudioRead.js
@@ -4,5 +4,11 @@ const songs = [
     artist: "Nicki Minaj",
     cover: "scripts/RomanReloadedCover.jpg", // relative to HTML
     src: "scripts/RomanReloaded.mp3" // relative to HTML
+  },
+    {
+    title: "Touch",
+    artist: "Katseye",
+    cover: "scripts/RomanReloadedCover.jpg", // relative to HTML
+    src: "scripts/RomanReloaded.mp3" // relative to HTML
   }
 ];
