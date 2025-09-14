332441bff0a1b397cf7ed132dd0cff7e22a90508
slygem123 <albin@grenhall.se>
2025-09-14 22:23:53 +0200

    Update AudioRead.js

--- scripts/AudioRead.js
+++ scripts/AudioRead.js
@@ -1,9 +1,8 @@
-// scripts/AudioRead.js
 const songs = [
   {
     title: "Dreams",
     artist: "Joakim Karud",
-    cover: "AudioRead/cover1.png",
-    src: "scripts/RomanReloaded.mp3"
+    cover: "AudioRead/cover1.png", // relative to HTML
+    src: "scripts/RomanReloaded.mp3" // relative to HTML
   }
 ];
